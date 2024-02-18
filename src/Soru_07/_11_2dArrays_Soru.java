60cc398877cbe4d1d67e3c7d2017e8b975137335
Ramazaninceli <inceliramazan80@gmail.com>
2024-02-18 11:13:13 +0300

    2dArraysMiniProject_1 commit

---
+++ src/Soru_07/_11_2dArrays_Soru.java
@@ -0,0 +1,37 @@
+package Soru_07;
+
+import java.util.Arrays;
+
+public class _11_2dArrays_Soru {
+    public static void main(String[] args) {
+        // 2 ye 3 lük bir tabloyu random 100 e kadar sayılarla doldurunuz
+        // bütün doldurma işlemi bittikten sonra
+        // sonrasında yazdırınız ve kaç tane tek sayı olduğunu bulunuz
+
+        int[][] table = new int[2][3];
+
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 3; j++) {
+                table[i][j] = (int) (Math.random() * 10);
+
+            }
+        }
+        for (int i = 0; i < table.length; i++) {
+            for (int j = 0; j < table[i].length; j++) {
+               System.out.print(table[i][j]);
+            }
+            System.out.println();
+        }
+        int count = 0;
+        for (int i = 0; i < table.length; i++) {
+            for (int j = 0; j < table[i].length; j++) {
+                if (table[i][j] % 2 != 0) {
+                    count++;
+                }
+
+            }
+
+        }
+        System.out.println("Tablodaki tek sayı miktarı = " + count);
+    }
+}
---
+++ src/Soru_07/_12_2dArraysSoru.java
@@ -0,0 +1,38 @@
+package Soru_07;
+
+import java.util.Scanner;
+
+public class _12_2dArraysSoru {
+    public static void main(String[] args) {
+        // 2x3 lük bir tabloyu random 10 a kadar sayılarla doldurunuz.
+        // Daha sonra kullanıcıdan bir sayı alınız.Bu sayı tabloda
+        // var ise yerini (satir,sütun) yazdırınız.
+        int[][] table = new int[2][3];
+
+        for (int i = 0; i < table.length; i++) {
+            for (int j = 0; j < table[i].length; j++) {
+                table[i][j] = (int) (Math.random() * 10);
+                System.out.print(table[i][j]);
+            }
+            System.out.println();
+        }
+        Scanner input = new Scanner(System.in);
+        System.out.print("Bir sayı giriniz: ");
+        int number = input.nextInt();
+
+        for (int i = 0; i < table.length; i++) {
+            for (int j = 0; j < table[i].length; j++) {
+
+                if (table[i][j] == number){
+
+                    System.out.println("Girmiş olduğnuz sayının tablodaki indexi: " + i+""+j);}
+
+            }
+
+
+        }
+
+    }
+
+}
+
