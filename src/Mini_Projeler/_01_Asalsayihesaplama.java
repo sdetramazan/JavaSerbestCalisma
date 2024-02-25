a39c54ae491f6e7fc36e744d67de69a77bfeab64
Ramazaninceli <inceliramazan80@gmail.com>
2024-02-25 18:52:28 +0300

    MiniProject_01 commit

---
+++ src/Mini_Projeler/_01_Asalsayihesaplama.java
@@ -0,0 +1,40 @@
+package Mini_Projeler;
+
+import java.util.Scanner;
+
+public class _01_Asalsayihesaplama {
+    public static void main(String[] args) {
+        //Kullanıcının gireceği sayının Asal sayı olup olmadığını bulan program yapınız.
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Sayı giriniz: ");
+        int number = input.nextInt();
+
+        if ((number < 2)) {
+            System.out.println("2 den küçük sayı girilemez");
+            return;
+        }
+        for (int i = 1; i < number; i++) {
+
+
+            if (number == 2) {
+                System.out.println(number + " çift asal sayıdır");
+                break;
+            }
+
+            int value = number % (number - i);
+            if (value == 0) {
+                System.out.println(number + " asal sayı değil .");
+                break;
+            }
+            if (number == (i + 2) && (value > 0)) {
+
+                System.out.println(number + " asal sayı ");
+                break;
+            }
+        }
+    }
+}
+
+
+
